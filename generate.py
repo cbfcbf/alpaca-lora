dbb4ca04577449507ca1b4d748ed028cdfda8c24
cbfcbf <cbfcbf.byron@gmail.com>
2024-06-11 22:02:22 +0800

    1

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 # BASE_MODEL='huggyllama/llama-7b'
 # LORA_WEIGHT="tloen/alpaca-lora-7b"
 
-# python generate.py  --load_8bit  --base_model 'decapoda-research/llama-7b-hf'  --lora_weights 'tloen/alpaca-lora-7b'
+# python generate.py  --load_8bit  --base_model 'huggyllama/llama-7b'  --lora_weights 'tloen/alpaca-lora-7b'
 
 
 import os
